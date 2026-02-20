7451ef5d9a9d9fae48913f6accbda11271161699
imafancydev <lucasaugustoleon@gmail.com>
2026-02-19 21:05:15 -0300

    feat: add bold ANSI support to color output

--- main.c
+++ main.c
@@ -4,6 +4,29 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <unistd.h>
+#include "cinit.h"
+
+static const char *cinit_color_to_ansi(cinit_color_t color)
+{
+    switch (color)
+    {
+        case CINIT_COLOR_RED:     return "\033[1;31m";
+        case CINIT_COLOR_GREEN:   return "\033[1;32m";
+        case CINIT_COLOR_YELLOW:  return "\033[1;33m";
+        case CINIT_COLOR_BLUE:    return "\033[1;34m";
+        case CINIT_COLOR_MAGENTA: return "\033[1;35m";
+        case CINIT_COLOR_CYAN:    return "\033[1;36m";
+        case CINIT_COLOR_WHITE:   return "\033[1;37m";
+        default:
+            return "\033[1;37m";
+    }
+}
+
+void cinit_put_str_color(const char *str, cinit_color_t color)
+{
+    printf("%s%s\033[0m", cinit_color_to_ansi(color), str);
+}
 
 int main_file(const char *project_name) {
   char main_path[256];
@@ -11,7 +34,7 @@ int main_file(const char *project_name) {
   FILE *f = fopen(main_path, "w");
 
   if (f == NULL) {
-    printf("Error creating main.c\n");
+    cinit_put_str_color("Error creating main.c\n", 1);
     return 1;
   }
 
@@ -31,7 +54,7 @@ int readme(const char *project_name) {
   FILE *f = fopen(readme_path, "w");
 
   if (f == NULL) {
-    printf("Error creating README.md\n");
+    cinit_put_str_color("Error creating README.md\n", 1);
     return 1;
   }
 
@@ -48,7 +71,7 @@ int gitignore(const char *project_name) {
   FILE *f = fopen(ignore_path, "w");
 
   if (f == NULL) {
-    printf("Error creating gitignore\n");
+    cinit_put_str_color("Error creating gitignore\n", 1);
     return 1;
   }
 
@@ -64,7 +87,7 @@ int makefile(const char *project_name) {
   FILE *f = fopen(make_path, "w");
 
   if (f == NULL) {
-    printf("Error creating Makefile\n");
+    cinit_put_str_color("Error creating Makefile\n", 1);
     return 1;
   }
 
@@ -85,14 +108,14 @@ int main(int argc, char *argv[]) {
     if (strcmp(argv[1], "new") == 0) {
       size_t length = strlen(argv[2]);
       if (length <= 3) {
-        printf("Your project name needs to be longer than 3\n");
+        cinit_put_str_color("Your project name needs to be longer than 3\n", 1);
         return 1;
       }
 
       // creating dir
       int dir = mkdir(argv[2], 0755);
       if (dir != 0) {
-        printf("An error occurs creating your project\n");
+        cinit_put_str_color("An error occurs creating your project\n", 1);
         return 1;
       }
       char src_path[256];
@@ -102,36 +125,37 @@ int main(int argc, char *argv[]) {
       // creating main
       int mf = main_file(argv[2]);
       if (mf != 0) {
-        printf("An error occurs creating your main file\n");
+        cinit_put_str_color("An error occurs creating your main file\n", 1);
         return 1;
       }
       // creating readme
       int rm = readme(argv[2]);
       if (rm != 0) {
-        printf("An error occurs creating your Readme\n");
+        cinit_put_str_color("An error occurs creating your Readme\n", 1);
         return 1;
       }
 
       // creating ignore
       int ig = gitignore(argv[2]);
       if (ig != 0) {
-        printf("An error occurs creating gitignore\n");
+        cinit_put_str_color("An error occurs creating gitignore\n", 1);
         return 1;
       }
 
       // creating makefile
       int mk = makefile(argv[2]);
       if (mk != 0) {
-        printf("An error occurs creating makefile\n");
+        cinit_put_str_color("An error occurs creating makefile\n", 1);
         return 1;
       }
     }
-    printf("%s was created!\n", argv[2]);
+    char success_msg[256];
+    snprintf(success_msg, sizeof(success_msg), "%s was created!\n", argv[2]);
+    cinit_put_str_color(success_msg, CINIT_COLOR_GREEN);
     return 0;
   } else {
-
-    printf("Arguments are required for creating a new project\n");
-    printf("Try: cinit new project_name\n");
+    cinit_put_str_color("Arguments are required for creating a new project\n", 1);
+    cinit_put_str_color("Try: cinit new project_name\n", 1);
     return 1;
   }
 }
